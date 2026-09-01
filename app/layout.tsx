ad25b380fd19717ed2fa2154229e9048f8504e15
Seu Nome <seu-email@exemplo.com>
2026-08-31 23:44:43 -0400

    Configure Meta Pixel page view tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,12 +35,14 @@ export default function RootLayout({
     <html lang="pt-BR">
       <head>
         <script dangerouslySetInnerHTML={{__html:`(()=>{if('scrollRestoration'in history)history.scrollRestoration='manual';if(location.hash)history.replaceState(null,'',location.pathname+location.search);scrollTo(0,0);addEventListener('DOMContentLoaded',()=>scrollTo(0,0),{once:true})})()`}} />
+        <script dangerouslySetInnerHTML={{__html:`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','813194448363424');fbq('track','PageView');`}} />
         <link rel="preload" as="image" href="/hero-manchas-zero-mobile.webp" media="(max-width: 720px)" fetchPriority="high" />
         <link rel="preload" as="image" href="/hero-manchas-zero.webp" media="(min-width: 721px)" fetchPriority="high" />
       </head>
       <body
         className={`${bricolageGrotesque.variable} antialiased`}
       >
+        <noscript><img height="1" width="1" style={{display:'none'}} src="https://www.facebook.com/tr?id=813194448363424&ev=PageView&noscript=1" alt="" /></noscript>
         {children}
       </body>
     </html>
